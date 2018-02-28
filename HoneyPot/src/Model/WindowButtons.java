9c4030bafc985e2b41cd013567aab3b92b392da1
JDC <jeanpierre.decuba@student.fontys.nl>
2018-02-28 15:45:46 +0100

    part 2

--- HoneyPot/src/Model/WindowButtons.java
+++ HoneyPot/src/Model/WindowButtons.java
@@ -1,4 +1,24 @@
 package Model;
 
-public class WindowButtons {
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+public class WindowButtons extends HBox {
+
+    public WindowButtons() {
+        Button closeBtn = new Button("x");
+        closeBtn.setStyle("-fx-background-color: transparent; -fx-text-fill: achmeaPink; -fx-font-family: Roboto; -fx-font-size: 22; -fx-font-weight: bold");
+        closeBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Platform.exit();
+            }
+        });
+
+        this.getChildren().add(closeBtn);
+    }
 }
