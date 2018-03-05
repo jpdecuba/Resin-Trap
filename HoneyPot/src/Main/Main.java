4c3f00368e5bae28153fdbec3dd77b4f8ef3e896
JDC <jeanpierre.decuba@student.fontys.nl>
2018-03-05 14:59:46 +0100

    added language support

--- HoneyPot/src/Main/Main.java
+++ HoneyPot/src/Main/Main.java
@@ -12,16 +12,21 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
-    Stage Stage;
+    static Stage Stage;
     private double xOffset = 0;
     private double yOffset = 0;
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/OverView.fxml"));
+        loader.setResources(ResourceBundle.getBundle("bundles.UIResources", new Locale("en", "EN")));
         Parent root = loader.load();
         OverviewController overview = loader.getController();
         this.Stage = primaryStage;
+        overview.setStageAndSetupListeners(Stage);
         Stage.initStyle(StageStyle.UNDECORATED);
         Scene scene = new Scene(root);
         scene.getStylesheets().add("http://fonts.googleapis.com/css?family=Roboto");
@@ -49,5 +54,12 @@ public class Main extends Application {
             }
         });
         Stage.show();
+    }
+
+    public static void switchPage(Parent parent, String title)
+    {
+        Main.Stage.getScene().setRoot(parent);
+        Main.Stage.setTitle(title);
+        Main.Stage.show();
     }
 }
