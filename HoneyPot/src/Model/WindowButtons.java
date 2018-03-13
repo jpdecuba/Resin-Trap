924e8094d2bf6b9e724d34fc6612f5fb42cf51eb
Thomas Horvers <thorvers@xs4all.nl>
2018-03-13 12:50:16 +0100

    Maximize/minimize

--- HoneyPot/src/Model/WindowButtons.java
+++ HoneyPot/src/Model/WindowButtons.java
@@ -20,9 +20,12 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.util.Locale;
 
 public class WindowButtons extends HBox {
+
+    boolean fullscreen;
 
     public WindowButtons() {
         Button closeBtn = new Button("");
@@ -59,17 +62,22 @@ public class WindowButtons extends HBox {
         fa.setFill(Color.valueOf("#c15683"));
         maxBtn.setGraphic(fa);
         fa.setSize("22");
+
+        fullscreen = false;
+
         maxBtn.setStyle("-fx-background-color: transparent; -fx-text-fill: achmeaPink; -fx-alignment: baseline-center");
         maxBtn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent actionEvent) {
-                Screen screen = Screen.getPrimary();
-                Rectangle2D bounds = screen.getVisualBounds();
-                stage.setX(bounds.getMinX());
-                stage.setY(bounds.getMinY());
-                stage.setWidth(bounds.getWidth());
-                stage.setHeight(bounds.getHeight());
+                if (!fullscreen) {
+                    fullscreen = true;
+                    stage.setMaximized(true);
+                }
+                else {
+                    fullscreen = false;
+                    stage.setMaximized(false);
+                }
             }
         });
 
