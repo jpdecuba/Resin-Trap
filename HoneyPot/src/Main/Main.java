473b3b1943b512e3b232737e206a4dec01d064a0
Sjors Scholten <s.scholten@student.fhict.nl>
2018-03-28 16:12:05 +0200

    Auto stash before merge of "master" and "origin/master"

--- HoneyPot/src/Main/Main.java
+++ HoneyPot/src/Main/Main.java
@@ -74,8 +74,8 @@ public class Main extends Application {
 		this.Stage = primaryStage;
 		toolbar = new JFXToolbar();
         this.manager = new ControllerManager();
-		manager.currentView = "/View/OverView.fxml";
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/OverView.fxml"));
+		manager.currentView = "/View/PresetView.fxml";
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/PresetView.fxml"));
         loader.setResources(ResourceBundle.getBundle("bundles.UIResources", new Locale(lang, lang.toUpperCase())));
         Parent root = loader.load();
         this.Stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/Resources/Ducky.png")));
@@ -121,10 +121,6 @@ public class Main extends Application {
 
     public static void StartHoneypotServices(List<LIModule> list){
         Services.addAll(list);
-        for(LIModule item : Services){
-            honeypot.RegisterService(item);
-            honeypot.startPort(item.getPort());
-        }
     }
 
 
--- HoneyPot/src/Model/Preset/Preset.java
+++ HoneyPot/src/Model/Preset/Preset.java
@@ -2,10 +2,12 @@ package Model.Preset;
 
 import HoneyPot.lowinteraction.LIModule;
 import HoneyPot.protocol.FtpProtocol;
+import HoneyPot.protocol.IrcProtocol;
 import HoneyPot.protocol.MySQLProtocol;
 import HoneyPot.protocol.SmtpProtocol;
 import Main.Main;
 
+import javax.naming.LimitExceededException;
 import java.util.ArrayList;
 
 public class Preset {
@@ -25,20 +27,47 @@ public class Preset {
 
     private void FillServices(){
         services.clear();
+        LIModule mSQL = new LIModule(new MySQLProtocol(), Main.honeypot);
+        LIModule mFTP = new LIModule(new FtpProtocol(), Main.honeypot);
+        LIModule mIRC = new LIModule(new IrcProtocol(), Main.honeypot);
+        LIModule mSMTP = new LIModule(new SmtpProtocol(), Main.honeypot);
+
+        services.add(mSQL);
+        services.add(mFTP);
+        services.add(mIRC);
+        services.add(mSMTP);
+
+        for (LIModule m: services) {
+            Main.honeypot.RegisterService(m);
+            Main.honeypot.startPort(m.getPort());
+        }
+
         switch(soort){
             case Express:
                 //Express function
                 return;
             case Home:
-                services.add(new LIModule(new FtpProtocol(), Main.honeypot));
+                for(LIModule m : services){
+                    if(m == mSQL){ Main.honeypot.DeRegisterService(m); }
+                    else if(m == mSMTP){ Main.honeypot.DeRegisterService(m); }
+                    else if(m == mIRC){ Main.honeypot.DeRegisterService(m); }
+                }
                 break;
             case Company_Small:
                 switch (type) {
                     case Type_1:
-                        services.add(new LIModule(new SmtpProtocol(), Main.honeypot));
+                        for(LIModule m : services){
+                            if(m == mIRC){ Main.honeypot.DeRegisterService(m); }
+                            else if(m == mSQL){ Main.honeypot.DeRegisterService(m); }
+                            else if(m == mFTP){ Main.honeypot.DeRegisterService(m); }
+                        }
                         break;
                     case Type_2:
-                        services.add(new LIModule(new MySQLProtocol(), Main.honeypot));
+                        for(LIModule m : services){
+                            if(m == mFTP){ Main.honeypot.DeRegisterService(m); }
+                            else if(m == mSMTP){ Main.honeypot.DeRegisterService(m); }
+                            else if(m == mIRC){ Main.honeypot.DeRegisterService(m); }
+                        }
                         break;
                         default:
                             return;
@@ -47,12 +76,16 @@ public class Preset {
             case Company_Big:
                 switch (type) {
                     case Type_1:
-                        services.add(new LIModule(new MySQLProtocol(), Main.honeypot));
-                        services.add(new LIModule(new FtpProtocol(), Main.honeypot));
+                        for(LIModule m : services){
+                            if(m == mIRC){ Main.honeypot.DeRegisterService(m); }
+                            else if(m == mSQL){ Main.honeypot.DeRegisterService(m); }
+                        }
                         break;
                     case Type_2:
-                        services.add(new LIModule(new SmtpProtocol(), Main.honeypot));
-                        services.add(new LIModule(new FtpProtocol(), Main.honeypot));
+                        for(LIModule m : services){
+                            if(m == mIRC){ Main.honeypot.DeRegisterService(m); }
+                            else if(m == mSMTP){ Main.honeypot.DeRegisterService(m); }
+                        }
                         break;
                     default:
                         return;
