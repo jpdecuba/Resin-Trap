179055d1026c9247e89184b44193c52000469e69
JDC <jeanpierre.decuba@student.fontys.nl>
2018-04-03 09:47:35 +0200

    added admin view and controller

--- HoneyPot/src/Main/Main.java
+++ HoneyPot/src/Main/Main.java
@@ -74,8 +74,8 @@ public class Main extends Application {
 		this.Stage = primaryStage;
 		toolbar = new JFXToolbar();
         this.manager = new ControllerManager();
-		manager.currentView = "/View/OverView.fxml";
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/OverView.fxml"));
+		manager.currentView = "/View/PresetView.fxml";
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/PresetView.fxml"));
         loader.setResources(ResourceBundle.getBundle("bundles.UIResources", new Locale(lang, lang.toUpperCase())));
         Parent root = loader.load();
         this.Stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/Resources/Ducky.png")));
