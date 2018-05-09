dbc8e3d8cab3040c9324f8c039e77634cce7223a
Dotter <bart.van.dongen@hotmail.com>
2018-05-09 10:29:40 +0200

    Working Log files from db

--- HoneyPot/src/Client/HoneyPot/lowinteraction/LIDeserializeDBThread.java
+++ HoneyPot/src/Client/HoneyPot/lowinteraction/LIDeserializeDBThread.java
@@ -5,6 +5,7 @@ package Client.HoneyPot.lowinteraction;
 import Client.HoneyPot.Repository.LogDatabase;
 import Client.HoneyPot.Repository.LogRepository;
 import Client.HoneyPot.logging.LogConnection;
+import Client.Main.Main;
 
 import java.util.Set;
 import java.util.concurrent.Callable;
@@ -12,7 +13,6 @@ import java.util.concurrent.Callable;
 public class LIDeserializeDBThread implements Callable<Set<LogConnection>> {
     @Override
     public Set<LogConnection> call() throws Exception {
-        LogRepository repository = new LogRepository(new LogDatabase());
-        return repository.GetAllLogs();
+        return Main.GetLogs();
     }
 }
