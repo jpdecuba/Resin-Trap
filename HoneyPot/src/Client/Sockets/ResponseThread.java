9d1d07d5ad655832a1a31456fb5c7639c935c023
Dotter <bart.van.dongen@hotmail.com>
2018-05-30 10:45:05 +0200

    socket server2

---
+++ HoneyPot/src/Client/Sockets/ResponseThread.java
@@ -0,0 +1,47 @@
+package Client.Sockets;
+
+import Shared.Request.Response;
+import javafx.beans.Observable;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.Observer;
+
+public class ResponseThread extends Thread  {
+
+    protected ObjectInputStream input;
+    private SocketObserver observer;
+
+    public ResponseThread(ObjectInputStream input, SocketObserver observer) {
+        this.input = input;
+        this.observer = observer;
+    }
+
+    public boolean run = true;
+
+    @Override
+    public void run() {
+
+        try {
+            System.err.println("Connecting started");
+
+            while (run) {
+
+                System.out.println("in loop");
+                Object obj = input.readObject();
+                Response response = (Response)obj;
+
+                observer.object(response);
+
+            }
+            input.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
